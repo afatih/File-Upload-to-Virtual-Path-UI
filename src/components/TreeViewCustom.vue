1403086b3292b0e7e6cf27005fcfd9163d4a79f9
Fatih <eem.fatih@outlook.com>
2019-12-22 22:02:59 +0300

    Add TreeView loading error notify

--- src/components/TreeViewCustom.vue
+++ src/components/TreeViewCustom.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-    <tree-view  :tree="tree" ></tree-view>
+    <tree-view :tree="tree"></tree-view>
   </div>
 </template>
 
 <script>
-
-import TreeView from '../treeview.vue';
+import TreeView from "../treeview.vue";
 
 export default {
-  components:{
-    "tree-view":TreeView
+  components: {
+    "tree-view": TreeView
   },
   name: "treeviewcustom",
   data() {
@@ -20,13 +19,23 @@ export default {
   },
   methods: {
     refreshNodes: function() {
-      this.$http.get("http://localhost:2525/api/nodes").then(function(data) {
-        this.tree = JSON.parse(JSON.stringify(data.body));
-      });
+      this.$http
+        .get("http://localhost:2525/api/nodes")
+        .then(function(data) {
+          this.tree = JSON.parse(JSON.stringify(data.body));
+        })
+        .catch(e => {
+          this.$notify({
+            group: "foo",
+            title: "Error",
+            type: "error",
+            text: "Error Message=" + e
+          });
+        });
     }
   },
   mounted() {
-        this.refreshNodes();
+    this.refreshNodes();
   }
 };
 </script>
